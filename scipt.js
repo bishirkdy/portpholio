97576fdc95bd4c2948083e691910611ed4ebb228
bishirkdy <bishirbishi1@gmail.com>
2026-03-08 13:37:59 +0530

    added hunburger intraction and cv and hire button intraction

--- scipt.js
+++ scipt.js
@@ -15,10 +15,30 @@ form.addEventListener("submit", function (e) {
     .send("service_wbh20rj", "template_126vnxt", data)
     .then((res) => {
       console.log("Email sent successfully", res);
-
       form.reset();
     })
     .catch((err) => {
       console.log("Error sending email", err);
     });
+});
+
+const navMenu = document.getElementById("navMenu");
+const menuBtn = document.getElementById("menuBtn");
+
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
+
+const hireBtn = document.getElementById("hire-btn");
+const cvBtn = document.getElementById("cv-btn");
+const contactSection = document.getElementById("contact");
+
+hireBtn.addEventListener("click", () => {
+  alert("Could you mail me? Let's keep in touch!");
+  contactSection.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+cvBtn.addEventListener("click", () => {
+  alert("CV not added yet. It will be available soon. Stay tuned!");
 });
